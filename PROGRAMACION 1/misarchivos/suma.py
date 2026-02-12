62e7531a5995cde084e74a25cef78c7386bccee7
Santiago Arquez <santiagoe-arquezc@unilibre.edu.co>
2026-02-12 13:23:10 +0000

    7

--- PROGRAMACION 1/misarchivos/suma.py
+++
@@ -1,9 +0,0 @@
-print("Ingresa números para sumarlos:")
-x = 0
-while True:
-    try:
-        num = float(input("Número (o 'salir' para terminar): "))
-        x += num
-    except ValueError:
-        print("Suma total:", x)
-        break
