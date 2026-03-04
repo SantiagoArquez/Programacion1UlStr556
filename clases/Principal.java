3a6b09a717a66a41d1be2c466d49f9b5b26188a7
Santiago Arquez <santiagoe-arquezc@unilibre.edu.co>
2026-03-04 18:08:44 +0000

    $

--- clases/Principal.java
+++ clases/Principal.java
@@ -6,5 +6,5 @@ public class Principal{
         corriente.mostrarDatosC();
     }
 }
-//crear contructores y sobre cargarlos
+//crear constructores y sobre cargarlos
 //padre hijo clase atributos encapula 2 constructores y los sobrecarga
